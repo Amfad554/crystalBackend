c2b84bfff2eaa71ea45103d86df10ef0c1e160bd
Favour <georgechiamaka02@gmail.com>
2025-12-27 21:17:03 +0100

    pushing to render2

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,5 @@
-const prisma = require("../config/db");
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
 const bcrypt = require('bcrypt');
 const jwt = require("jsonwebtoken");
 const { sendVerification } = require("../utils/emailVerification");
