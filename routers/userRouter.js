951c1810926f376f545d420c18b9d770e509107f
Favour <georgechiamaka02@gmail.com>
2026-02-16 14:19:33 +0100

    added image from admin to equipment

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
-const { isUser } = require('../middleware/authMiddleware'); // Use your existing middleware
+const { isUser } = require('../middleware/auth'); // Use your existing middleware
 const upload = require('../middleware/multer'); // Use your existing multer
 
 // Public routes
